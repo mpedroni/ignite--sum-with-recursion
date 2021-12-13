f8e102b19ff9cce38cc64f8bbd44e0b9beaad284
Matheus Pedroni <mpedroni77@gmail.com>
2021-12-13 14:12:13 -0300

    :tada: entrega do desafio

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,13 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    count_list_items(list, 0)
   end
+
+  defp count_list_items([_head | tail], acc) do
+    acc = acc + 1
+
+    count_list_items(tail, acc)
+  end
+
+  defp count_list_items([], acc), do: acc
 end
